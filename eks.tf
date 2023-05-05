d2222a3816122bbf251ffd4d134ed6a1a2171a92
Damian Tykalowski <damian@d47zm3.me>
2023-05-05 11:53:58 +0200

    Check EKS Workaround

--- eks.tf
+++ eks.tf
@@ -123,6 +123,7 @@ module "eks" {
 
   # aws-auth configmap
   manage_aws_auth_configmap = true
+  create_aws_auth_configmap = true
   aws_auth_roles            = flatten(concat(local.karpenter_aws_auth_role, var.aws_auth_roles))
 
   ## Tags
