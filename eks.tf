198fc5aeffa4545ab7c5faf3f3d86548a7448961
Damian Tykalowski <damian@d47zm3.me>
2023-06-15 14:44:27 +0200

    don't manage aws auth - revert

--- eks.tf
+++ eks.tf
@@ -122,7 +122,7 @@ module "eks" {
   kms_key_description     = "KMS Key for Kubernetes Secrets Encryption"
 
   # aws-auth configmap
-  manage_aws_auth_configmap = false
+  manage_aws_auth_configmap = true
   aws_auth_roles            = flatten(concat(local.karpenter_aws_auth_role, var.aws_auth_roles))
 
   ## Tags
