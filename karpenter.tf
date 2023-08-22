00dd50cd93fe0782f5867d3a67f0e114876c6c55
Damian Tykalowski <damian@d47zm3.me>
2023-08-22 14:59:22 +0200

    Test Karpenter Instance Profile

--- karpenter.tf
+++ karpenter.tf
@@ -8,12 +8,13 @@ module "karpenter" {
   irsa_oidc_provider_arn          = module.eks.oidc_provider_arn
   irsa_namespace_service_accounts = ["karpenter:karpenter"]
 
-  create_irsa             = true
-  irsa_name               = "${var.cluster_name}-KarpenterIRSA"
-  irsa_use_name_prefix    = false
-  create_instance_profile = true
-  create_iam_role         = false
-  iam_role_arn            = module.eks.eks_managed_node_groups["spot"].iam_role_arn
+  create_irsa              = true
+  irsa_name                = "${var.cluster_name}-KarpenterIRSA"
+  irsa_use_name_prefix     = false
+  create_instance_profile  = true
+  create_iam_role          = false
+  iam_role_use_name_prefix = false
+  iam_role_arn             = module.eks.eks_managed_node_groups["spot"].iam_role_arn
 
   tags = merge(
     var.tags
