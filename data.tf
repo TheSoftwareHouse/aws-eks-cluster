3a1b69a42aa844badcd045566c81715d07943dc4
Damian Tykalowski <damian@d47zm3.me>
2023-08-31 16:06:50 +0200

    Kubernetes Provider

--- data.tf
+++ data.tf
@@ -1,7 +1,4 @@
 data "aws_caller_identity" "current" {}
-data "aws_eks_cluster" "eks" {
-  name = module.eks.cluster_name
-}
 data "aws_eks_cluster_auth" "eks" {
   name = module.eks.cluster_name
 }
