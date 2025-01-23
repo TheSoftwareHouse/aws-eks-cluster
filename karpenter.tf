532556b8833ae98e9bd7c616674e20a32da0cf40
Damian Tykalowski <damian.tykalowski@tsh.io>
2025-01-23 14:52:31 +0100

    replace karpenter role

--- karpenter.tf
+++ karpenter.tf
@@ -3,7 +3,9 @@ module "karpenter" {
   source  = "terraform-aws-modules/eks/aws//modules/karpenter"
   version = "20.26.1"
 
-  cluster_name = var.cluster_name
+  cluster_name            = var.cluster_name
+  enable_irsa             = true
+  create_instance_profile = true
 
   node_iam_role_additional_policies = {
     AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
