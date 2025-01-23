0327d8282642d7d51c30fdeac6f3e223f4b68790
Damian Tykalowski <damian.tykalowski@tsh.io>
2025-01-23 13:34:41 +0100

    bump modules

--- eks.tf
+++ eks.tf
@@ -154,7 +154,7 @@ locals {
 module "iam_role_for_service_account" {
   for_each = local.irsa_roles
   source   = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version  = "5.30.0"
+  version  = "5.52.2"
 
   role_name                              = "${each.value.name}IRSA-${var.cluster_name}"
   attach_load_balancer_controller_policy = lookup(each.value, "attach_load_balancer_controller_policy", false)
